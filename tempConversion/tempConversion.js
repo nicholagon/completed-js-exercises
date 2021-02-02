521cf1025e845626712d444c942be3b4ecb8c817
_weirdatlast <nicholagon@gmail.com>
2021-02-03 06:14:54 +0800

    Add tempConversion functions

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
+  
+  if(Number.isInteger(tempC))
+    return tempC;
+  else
+    return +tempC.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * (9 / 5)) + 32;
+  
+  if(Number.isInteger(tempF))
+    return tempF;
+  else
+    return +tempF.toFixed(1);
 }
 
 module.exports = {
