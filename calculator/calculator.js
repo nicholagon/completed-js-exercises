cbc933269d46460bd306680bbc09a998d6727542
_weirdatlast <nicholagon@gmail.com>
2021-02-20 01:08:35 +0800

    Add calculator solution with passing test cases

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
 function sum () {
-	
+	const numbers = [...arguments[0]];
+	const sum = numbers.reduce((total, number) => {
+		return total += number;
+	}, 0);
+	return sum;
 }
 
 function multiply () {
-	
+	const numbers = [...arguments[0]];
+	const product = numbers.reduce((total, number) => {
+		return total *= number;
+	});
+	return product;
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(number) {
+	let factorial = 1;
+	for(let i = 1; i <= number; i++) {
+		factorial *= i;
+	}
+	return factorial;
 }
 
 module.exports = {
