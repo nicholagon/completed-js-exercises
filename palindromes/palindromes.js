3a1a36099a48a99043ab3dfb6139965fb7c4056d
_weirdatlast <nicholagon@gmail.com>
2021-02-20 01:48:10 +0800

    Add solution for palindromes along with passing tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    let arr = word.toLowerCase().split("");
+    let origString = []
+    let newString = [];
 
+    for(let i = 0; i < arr.length; i++) {
+        if(isSmallLetter(arr[i]))
+            origString.push(arr[i]);
+    }
+
+    for(let i = arr.length - 1; i >= 0; i--) {
+        if(isSmallLetter(arr[i])){
+            newString.push(arr[i]);
+        }
+            
+    }
+    return origString.join() === newString.join();
+}
+
+function isSmallLetter(character) {
+    return (character >= 'a' && character <= 'z');
 }
 
 module.exports = palindromes
