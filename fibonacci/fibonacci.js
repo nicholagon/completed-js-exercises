db5b786045ce7b9968b2d3f77e3853021632fc32
_weirdatlast <nicholagon@gmail.com>
2021-02-21 23:42:58 +0800

    Add fibonacci solution with passing tests

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fib = []
+    let i = 0, j = 1, temp = 0;
+    
+    if(number <= 0) return "OOPS";
+    
+    while(fib.length !== +number) {
+        fib.push(j);
+        temp = i + j;
+        i = j;
+        j = temp;        
+    }
+    
+    return fib[fib.length - 1];
 }
 
 module.exports = fibonacci
