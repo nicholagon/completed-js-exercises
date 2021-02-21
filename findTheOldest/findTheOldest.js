4d5103f6c8c556be8988051e1ac88109c50f7229
_weirdatlast <nicholagon@gmail.com>
2021-02-22 00:21:46 +0800

    Add solution using sort function and make tests pass

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-let findTheOldest = function() {
+let findTheOldest = function(persons) {
+    const oldestSort = persons.sort((a, b) => {
+        let ageOfA, ageOfB;
 
+        if(!a.yearOfDeath)
+           ageOfA = getAge(a.yearOfBirth, new Date().getFullYear());
+        else
+            ageOfA = getAge(a.yearOfBirth, a.yearOfDeath);
+
+        if(!b.yearOfDeath)
+            ageOfB = getAge(b.yearOfBirth, new Date().getFullYear());
+        else
+            ageOfB = getAge(b.yearOfBirth, b.yearOfDeath);
+        
+        if(ageOfA > ageOfB) return -1;
+        if(ageOfA < ageOfB) return 1;        
+    })
+
+    return oldestSort[0];
+}
+
+function getAge(startYear, endYear) {
+    return endYear - startYear;
 }
 
 module.exports = findTheOldest
